fbfc25a76592391df97098783ccdb99cd5f783c3
Dave Lobban <d2_git_pc15@lobban-banff.co.uk>
2020-04-09 20:45:17 +0100

    Refactor

--- TestProject/DiamondBuilder.cs
+++ TestProject/DiamondBuilder.cs
@@ -19,69 +19,99 @@ namespace TestProject
 
             if (charIndex == 0)
             {
-                var letter = (char)65;
-                return new[] { letter.ToString() };
+                return new[] { "A" };
+            }
+
+            var output = GetSizedOutputArray(charIndex);
+            int totalLengthOfLine = GetLineLength(charIndex);
+
+            int outerPadding = charIndex;
+
+            bool beforeMiddle = true;
+            for (var i = 0; i <= 2 * charIndex; i++)
+            {
+                char letter = GetAlphabeticCharacterByAscii(charIndex, i);
+
+                if (letter == value)
+                {
+                    output[i] = AppendLineForInputLetter(charIndex, totalLengthOfLine, letter);
+                }
+                else
+                {
+                    output[i] = AppendLineForSurroundingLetter(totalLengthOfLine, outerPadding, i, letter);
+                }
+
+                outerPadding = GetOuterPadding(outerPadding, beforeMiddle);
+
+                if (outerPadding == 0)
+                {
+                    beforeMiddle = false;
+                }
+            }
+            return output;
+        }
+
+        private static char GetAlphabeticCharacterByAscii(int charIndex, int offset)
+        {
+            char letter;
+            if (offset <= charIndex)
+            {
+                letter = (char)(65 + offset);
             }
             else
             {
-                var output = new string[1 + (2 * charIndex)];
-                var totalLengthOfLine = (2 * (1 + charIndex)) - 1;
-
-                int outerPadding = charIndex;
-                
-                bool beforeMiddle = true;
-                for (var i = 0; i <= 2 * charIndex; i++)
-                {
-                    var padding = new string(' ', charIndex);
-
-                    char letter;
-                    if (i <= charIndex)
-                    {
-                        letter = (char)(65 + i);
-                    }
-                    else
-                    {
-                        letter = (char)(65 + Math.Abs((2 * charIndex) - i));
-                    }
-
-                    if (letter == value)
-                    {
-                        if (charIndex > 1)
-                        {
-                            padding = new string(' ', totalLengthOfLine-2);
-                        }
-
-                        output[i] = $"{letter}{padding}{letter}";
-                    }
-                    else
-                    {
-                        var secondPaddingLength = (totalLengthOfLine - 2) - (2 * outerPadding);
-                        secondPaddingLength = secondPaddingLength < 0 ? 0 : secondPaddingLength;
-
-                        var secondLetterCharacter = secondPaddingLength > 0 ? letter.ToString() : string.Empty;
-                        var secondLetter = i == 0 ? "" : $"{new string(' ', secondPaddingLength)}{secondLetterCharacter }";
-
-                        output[i] = $"{new string(' ', outerPadding)}{letter}{secondLetter}{new string(' ', outerPadding)}";
-                    }
-
-                    if (beforeMiddle)
-                    {
-                        outerPadding--;
-                    }
-                    else
-                    {
-                        outerPadding++;
-                    }
-
-                    if (outerPadding == 0)
-                    {
-                        beforeMiddle = false;
-                    }
-                }
-
-                return output;
-
+                letter = (char)(65 + Math.Abs((2 * charIndex) - offset));
             }
+
+            return letter;
+        }
+
+        private static int GetOuterPadding(int outerPadding, bool beforeMiddle)
+        {
+            if (beforeMiddle)
+            {
+                outerPadding--;
+            }
+            else
+            {
+                outerPadding++;
+            }
+
+            return outerPadding;
+        }
+
+        private static string AppendLineForSurroundingLetter(int totalLengthOfLine, int outerPadding, int i, char letter)
+        {
+            var secondPaddingLength = (totalLengthOfLine - 2) - (2 * outerPadding);
+            secondPaddingLength = secondPaddingLength < 0 ? 0 : secondPaddingLength;
+
+            var secondLetterCharacter = secondPaddingLength > 0 ? letter.ToString() : string.Empty;
+            var secondLetter = i == 0 ? "" : $"{new string(' ', secondPaddingLength)}{secondLetterCharacter }";
+
+            return $"{new string(' ', outerPadding)}{letter}{secondLetter}{new string(' ', outerPadding)}";
+            
+        }
+
+        private static string AppendLineForInputLetter(int charIndex, int totalLengthOfLine, char letter)
+        {
+            var padding = new string(' ', charIndex);
+
+            if (charIndex > 1)
+            {
+                padding = new string(' ', totalLengthOfLine - 2);
+            }
+
+            return $"{letter}{padding}{letter}";
+        }
+
+        private static string[] GetSizedOutputArray(int charIndex)
+        {
+            return new string[1 + (2 * charIndex)];
+        }
+
+        private static int GetLineLength(int charIndex)
+        {
+            return (2 * (1 + charIndex)) - 1;
         }
     }
 }
